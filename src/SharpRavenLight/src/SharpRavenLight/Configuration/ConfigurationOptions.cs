e22f8702888e0663b3d1fd33e4288d976c75f3dc
Mateus Demboski <mateus.demboski@shareprime.com.br>
2017-09-26 10:03:41 -0300

    Add Environment support

--- src/SharpRavenLight/src/SharpRavenLight/Configuration/ConfigurationOptions.cs
+++ src/SharpRavenLight/src/SharpRavenLight/Configuration/ConfigurationOptions.cs
@@ -3,5 +3,6 @@ namespace SharpRavenLight.Configuration
     public class ConfigurationOptions
     {
         public string dsn { get; set; }
+        public string environment { get; set; }
     }
 }
